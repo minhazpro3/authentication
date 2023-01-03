e6c52cbbbd51d1d947098b827db9e5cfc3ccd12d
minhazpro3 <minhazpro1@gmail.com>
2023-01-04 00:02:09 +0600

    change file

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const app = require("./app");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 require("dotenv").config();
+app.use(express.json());
+app.use(cors());
 const { dbConnection } = require("./utils/dbConnection");
+const router = require("./routes/v1/user_route");
 const PORT = process.env.PORT || 5000;
 
 // database connection
 dbConnection();
+
+app.use("/api/v1/user", router);
+
+app.get("/", (req, res) => {
+  res.send("Server started success");
+});
 app.all("*", (req, res) => {
   res.send("Could not find route");
 });
